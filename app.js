58986385cb243afc87f29922c1d85ed95c42721e
Anne-Ribeiro <84384217+Anne-Ribeiro@users.noreply.github.com>
2022-05-16 12:57:46 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const servidor = require("./config/servidor")
 //carregar o express
 const app = servidor.app
-//carregar a ports
+//carregar a porta
 const porta = servidor.porta
 
 //carregar a rota index
-const index = require('./routes/index')(app)
+//const index = require('./routes/index')(app)
+
+//importar o consign e configurar
+const consign = require('consign')
+consign().include('./routes').into(app)
 
 //ligar o servidor
 app.listen(porta)

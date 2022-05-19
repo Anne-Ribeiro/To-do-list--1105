6d50b587960baedfca538bc342c619f664ecf1b1
Anne-Ribeiro <84384217+Anne-Ribeiro@users.noreply.github.com>
2022-05-19 08:44:01 -0300

    Add files via upload

--- config/servidor.js
+++ config/servidor.js
@@ -10,6 +10,9 @@ const porta = process.env.PORT || 3030 // usado para pegar uma porta vazia
 //definir a pasta dos arquivos de css e imagens
 app.use(express.static('./assets'))
 
+//defir o express como body parse
+app.use(express.urlencoded({extended:false}))
+
 //exportar app e porta
 module.exports = {app,porta}
 

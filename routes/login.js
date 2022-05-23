722119925ac6f18831a88ff3637873a25e7ada6a
Anne-Ribeiro <84384217+Anne-Ribeiro@users.noreply.github.com>
2022-05-23 13:00:45 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -3,4 +3,25 @@ module.exports = (app)=>{
     app.get('/login',(req,res)=>{
         res.render('login.ejs')
     })
+
+    //validar usuario e senha
+    app.post('/login',async(req,res)=>{
+        //recuperar as informações digitadas no formulario
+        var dados = req.body
+        //conectar com o banco de dados
+        var database = require('../config/database')()
+        //selecionar a model usuarios
+        var usuarios = require('../models/usuarios')
+        //verificar se o email está cadastrado
+        var verificar = await usuarios.findOne({email:dados.email})
+        if(!verificar){
+            return res.send("Email não Cadastrado!")
+        }
+        var cript = require('bcryptjs')
+        var comparar = await  cript.compare(dados.senha, verificar.senha)
+        if(!comparar){
+            return res.send("Senha Inválida")
+        }
+        res.render("atividades.ejs")
+    })
 }
--- routes/registro.js
+++ routes/registro.js
@@ -12,11 +12,22 @@ module.exports = (app)=>{
         var database = require('../config/database')()
         //definir em qual coleção vamos gravar
         var usuarios = require('../models/usuarios')
+
+        //verificar se o email já está cadastrado
+        var verificar = await usuarios.findOne({email:dados.email})
+        if(verificar){
+           return res.send('Email já cadastrado')
+        }
+
+        //criptografar a senha
+        var cript = require("bcryptjs")
+        var senhasegura = await cript.hash(dados.senha,10)   
+
         //gravar o documento
         var documento = await new usuarios({
             nome: dados.nome,
             email: dados.email,
-            senha: dados.senha
+            senha: senhasegura
         }).save()
         res.redirect('/login')
     })

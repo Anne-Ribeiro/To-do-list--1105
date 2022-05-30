009847e288c84f9dfb4363858831d38636a19484
Anne-Ribeiro <84384217+Anne-Ribeiro@users.noreply.github.com>
2022-05-30 13:01:37 -0300

    Add files via upload

--- routes/atividades.js
+++ routes/atividades.js
@@ -1,6 +1,22 @@
 module.exports = (app) =>{
     app.post('/atividades',async(req,res)=>{
         var dados = req.body
-        return console.log(dados)
+        //return console.log(dados)
+
+        //conectar o mongo
+        const database = require("../config/database")()
+        //importar o model atividades
+        const atividades = require("../models/atividades")
+        //gravar as informações do formulário do database
+        var gravar = await new atividades({
+            data: dados.data,
+            tipo: dados.tipo,
+            entrega: dados.entrega,
+            disciplina: dados.disciplina,
+            instrucoes: dados.orientacoes,
+            usuario: dados.id
+        }).save()
+        //recarregar a página de atividades
+        res.render('atividades.ejs',{nome:dados.nome, id:dados.id})
     })
 }
